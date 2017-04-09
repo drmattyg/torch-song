cc85ae342be496e1475fd38f3bd675ade558292f
Matthew Gordon <inverarity@gmail.com>
2017-04-09 12:05:06 -0700

    Starting to build out out timings

--- songbook/songbook.py
+++ songbook/songbook.py
@@ -1,21 +1,25 @@
 import yaml
-from ..MCP.mcp import MCPInput
-import RPi.GPIO as GPIO
+
+from songbook.measure import Measure
+
 
 class Songbook:
+    def __init__(self, filename, calibration):
+        self.songbook = yaml.load(open(filename, "r").read())
+        self.timepoints = {}
+        self.calibration = calibration
 
-	def __init__(self, filename="conf/default.yml"):
-		self.conf = yaml.load(open(filename, "r").read())
-
-	def init_gpio(self):
-		GPIO.setmode(GPIO.BCM)
-		for edge in self.conf["edge"]:
-			for subsys in self.conf["subsystems"]["output"]:
-				if "gpio" in e[subsys]:
-					GPIO.setup(e[subsys], GPIO.OUT)
-			for subsys in self.conf["subsystems"]["input"]:
-				if "gpio" in e[subsys]:
-					GPIO.setup(e[subsys], GPIO.IN)
-
-	def init_mcp(self, mcp_config):
-		return MCPInput(mcp_config["addr"], mcp_config["range"])
+    def generating_timing_map(self):
+        for measure in self.songbook['songbook']:
+            start_time = measure['start_at']
+            if start_time not in self.timepoints:
+                self.timepoints[start_time] = []
+            for e in measure['edges']:
+                id = e['edge']
+                tx_valve = Measure.Transition(Measure.VALVE, id, e['flame'])
+                edge_calibration = self.calibration.get_calibration()[int(id)]
+                distance = e['distance'] if 'distance' in e else 1
+                t = float(measure['time'])
+                direction = int(e['dir'])
+                speed = edge_calibration.get_speed(t, distance=distance)
+                tx_dir = Measure.Transition(Measure.MOTOR, id, Measure.MotorState(direction, speed))
