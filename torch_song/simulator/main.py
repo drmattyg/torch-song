c8408feb052b7aac09f11d866778fdbaee802387
Matthew Gordon <inverarity@gmail.com>
2017-05-01 21:03:17 -0700

    Whee!

--- torch_song/simulator/main.py
+++ torch_song/simulator/main.py
@@ -7,8 +7,9 @@ import curses
 def main(scr):
     scr.nodelay(1)
     ts = SimTorchSong(scr)
-    for edge in ts.edges:
-        edge.motor_speed = 20
+    for ix, edge in enumerate(ts.edges):
+        edge.motor_speed = 80 - ix * 2
+        edge.position = ix / 20
         edge.valve = True
         edge.igniter = True
         edge.start()
@@ -16,6 +17,10 @@ def main(scr):
         while True:
             ts.render()
             time.sleep(0.1)
+            for edge in ts.edges:
+                if edge.limit_switches[0] or edge.limit_switches[1]:
+                    edge.motor_direction *= -1
+                    edge.valve = 1 - edge.valve
             s = scr.getstr()
             if s == "q":
                 break
