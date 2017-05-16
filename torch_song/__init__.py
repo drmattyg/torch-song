b472d0afb6d9ac525915a25a1beef9bdb1359b7a
Matthew Gordon <inverarity@gmail.com>
2017-05-15 22:04:10 -0700

    Remove stray reference

--- torch_song/__init__.py
+++ torch_song/__init__.py
@@ -1,5 +1,4 @@
 from .simulator import simulator
-from .torch_song_sim import torch_song_sim
 from .calibration import calibration
 from .limit_switch import limit_switch
 from .mcp23017 import mcp23017
