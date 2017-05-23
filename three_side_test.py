ff07a754619031d5645722ad579dec08c016d4a3
Matthew Gordon <inverarity@gmail.com>
2017-05-22 21:57:47 -0700

    Simple test songbook with curses

---
+++ three_side_test.py
@@ -0,0 +1,63 @@
+import curses
+import threading
+
+import time
+
+from torch_song.calibration import TSCalibration
+from torch_song.songbook import Songbook, SongbookRunner
+from torch_song.simulator import SimTorchSong
+
+SONGBOOK_YML = """
+songbook:
+  - start_at: 0
+    time: 4000
+    edges:
+      - edge: 0
+        flame: 1
+        dir: 1
+  - start_at: 4000
+    time: 4000
+    edges:
+      - edge: 0
+        flame: 0
+        dir: -1
+      - edge: 1
+        flame: 1
+        dir: 1
+  - start_at: 8000
+    time: 4000
+    edges:
+      - edge: 1
+        flame: 0
+        dir: -1
+      - edge: 2
+        flame: 1
+        dir: 1
+"""
+
+
+def main(scr):
+    sb = Songbook.from_string(SONGBOOK_YML, TSCalibration(None))
+    ts = SimTorchSong(scr, num_edges=3)
+    runner = SongbookRunner(sb, ts)
+    scr.nodelay(1)
+    SimTorchSong.initialize_curses()
+    th = threading.Thread(target=runner.run)
+    th.start()
+    try:
+        while True:
+            ts.render()
+            time.sleep(0.1)
+            s = scr.getstr()
+            if s == "q":
+                break
+        ts.kill()
+        th.join()
+
+    except KeyboardInterrupt:
+        ts.kill()
+        th.join()
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
