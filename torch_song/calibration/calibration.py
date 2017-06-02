4c491cf92634ea47fbd63971682587ca983b5409
Eyal <eyalc@uber.com>
2017-06-01 17:07:17 -0700

    calibration wip

--- torch_song/calibration/calibration.py
+++ torch_song/calibration/calibration.py
@@ -1,60 +1,81 @@
 import time
+import numpy
 
+def try_decorator(func):
+    timeout = 10
+    def wrap(*args, **kwargs):
+        then = time.time()
+        while ((time.time() - then) < timeout):
+            if (func(*args, **kwargs)):
+                return True
+        return False
+    return wrap
 
-class TSEdgeCalibration:
-    PLACEHOLDER_CALIBRATION = 4000.0
-    LIMIT_SWITCH_TEST_TIME = 0.2
+class EdgeCalibration:
+    MIN_SPEED = 50
+    CAL_SPEED_STEP = 5
 
-    def __init__(self, edge, calibration_travel_time=None):
-        """
-
-        :param id: Edge to calibrate
-        :param calibration_travel_time: End to end travel time in ms.
-        """
+    def __init__(self, edge):
         self.edge = edge
-        if calibration_travel_time is not None:
-            self.calibration_travel_time = float(calibration_travel_time)
-        else:
-            self.calibration_travel_time = float(self.run_calibration())
+        self.map_entries = []
+        self.fwd_speed_map = []
+        self.rev_speed_map = []
         self.polarity = True
 
     def get_speed(self, time, distance=1):
-        """
+        return int(numpy.interp(speed, self.map_entries, self.fwd_speed_map))
 
-        :param time: desired travel time, in milliseconds
-        :param distance: optional: travel distance from 0 to 1.  Default = 1
-        :return: Speed value for PWM from 0 to 255
-        """
-        # returns value from 0 to 255
-        return int(255.0 * distance * self.calibration_travel_time / time)
+    @try_decorator
+    def chk_beg_limit(self):
+        return self.edge.get_limit_switch_state()[0]
 
-    def run_calibration(self):
+    @try_decorator
+    def chk_end_limt(self):
+        return self.edge.get_limit_switch_state()[1]
+
+    @try_decorator
+    def chk_any_limt(self):
+        return any(self.edge.get_limit_switch_state())
+
+    def calibrate_one_speed(self, speed):
         e = self.edge
-        # make sure it's not pinned before we start
-        if any(e.get_limit_switch_state()):
-            e.set_motor_state(1, 150)
-            time.sleep(TSEdgeCalibration.LIMIT_SWITCH_TEST_TIME)
-            e.set_motor_state(1, 0)
-            if any(e.get_limit_switch_state()):
-                e.set_motor_state(-1, 150)
-                time.sleep(TSEdgeCalibration.LIMIT_SWITCH_TEST_TIME)
-                e.set_motor_state(1, 0)
-            if any(e.get_limit_switch_state()):
-                # try a bit faster
-                e.set_motor_state(1, 200)
-                time.sleep(TSEdgeCalibration.LIMIT_SWITCH_TEST_TIME)
-                e.set_motor_state(1, 0)
-            if any(e.get_limit_switch_state()):
-                e.set_motor_state(-1, 200)
-                time.sleep(TSEdgeCalibration.LIMIT_SWITCH_TEST_TIME)
-                e.set_motor_state(1, 0)
-            if any(e.get_limit_switch_state()):
-                raise Exception("Shit, my limit switch is stuck or some shit")
-        # todo: actual calibration
-        return TSEdgeCalibration.PLACEHOLDER_CALIBRATION
+        # move to start
+        e.set_motor_state(-1, 75)
+        if (not chk_beg_limit()):
+            e.set_motor_state(0, 0)
+            raise Exception('Calibration timeout')
+        e.set_motor_state(0, 0)
 
+        # go forward
+        then = time.time()
+        e.set_motor_state(1, speed)
+        if (not chk_end_limit()):
+            e.set_motor_state(0, 0)
+            raise Exception('Calibration timeout')
+        e.set_motor_state(0, 0)
+        fwd_time = time.time() - then
 
-class TSCalibration:
+        # go backward
+        then = time.time()
+        e.set_motor_state(-1, speed)
+        if (not chk_beg_limit()):
+            e.set_motor_state(0, 0)
+            raise Exception('Calibration timeout')
+        e.set_motor_state(0, 0)
+        rev_time = time.time() - then
+
+        return [rev_time, fwd_time]
+
+    def calibrate(self):
+        j = 0
+        for i in range(MIN_SPEED, 100+1, CAL_SPEED_STEP):
+            res = calibrate_one_speed(i)
+            self.map_entries[j] = i
+            self.fwd_speed_map[j] = res[0]
+            self.rev_speed_map[j] = res[1]
+            j = j + 1
+
+class Calibration:
     def __init__(self, config):
         self.config = config
         self.edge_map = {}
