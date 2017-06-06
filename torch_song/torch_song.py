6aebbc1119c27355d8d1bde7cc8c1dcfcc9edbc1
Matthew Gordon <inverarity@gmail.com>
2017-06-05 20:21:49 -0700

    Added a sleep to help prevent noise

--- torch_song/torch_song.py
+++ torch_song/torch_song.py
@@ -2,6 +2,7 @@ import yaml
 from torch_song.edge.real_edge import RealEdge
 from torch_song.hardware import MCPInput
 from torch_song.hardware import PCA9685
+import time
 
 
 class TorchSong:
@@ -17,6 +18,7 @@ class TorchSong:
         self.io['mcp23017'] = mcps
 
         self.edges = {i: RealEdge(i, self.io, self.config) for i in range(1, num_edges + 1)}
+        time.sleep(1)
 
     def calibrate(self):
         for e in self.edges.values():
