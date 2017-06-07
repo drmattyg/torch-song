324b7ae37417461c9f2b6c1b70ac39f30cf11759
Matthew Gordon <inverarity@gmail.com>
2017-06-06 21:33:55 -0700

    argle bargle

--- precom_main.py
+++ precom_main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-from torch_song import TorchSong
+from torch_song.torch_song import TorchSong
 from torch_song.songbook import Songbook
 from torch_song.songbook import SongbookRunner
 
 
 def main():
     ts = TorchSong(num_edges=1)
-    sb = Songbook.from_string("songbooks/one_edge_test.yml", ts.calibration)
+    sb = Songbook.from_string("songbooks/one_edge_test.yml", ts)
     runner = SongbookRunner(sb, ts)
-
-    ts.calibrate()
+    runner.run()
+    for e in ts.edges:
+        e.motor_driver.stop()
+        e.runner.join(5000)
 
 
 if __name__ == '__main__':
